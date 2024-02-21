968bd09365572460b7aeebe8f450c23248ab02a8
Lucas Souto <contato.lucassouto@outlook.com>
2024-02-21 16:52:18 -0300

    Pequena alteração.

--- ReduzYou/wwwroot/js/account_form.js
+++ ReduzYou/wwwroot/js/account_form.js
@@ -1,30 +1,32 @@
 ﻿const warningSign = document.getElementById("sign-warning");
-const validUser = /^[a-zA-Z0-9-_]+$/, validPass = /^[a-zA-Z0-9-_!?]+$/;;
+const validUser = /^[a-zA-Z0-9-_]+$/, validPass = /^[a-zA-Z0-9-_!?]+$/;
+
+function formSubmit(url, form, warning, success = empty_action)
+{
+    const formData = new FormData(form);
+    const username = formData.get("username"), password = formData.get("password");
+    warning.innerHTML = "";
+
+    if (username.length < 3) warning.innerHTML += "O nome de usuário deve conter pelo menos três caracteres!<br />";
+    else if (!validUser.test(username)) warning.innerHTML += "O nome de usuário só pode conter letras, números, \"-\" e \"_\".<br />";
+
+    if (password.length < 7) warning.innerHTML += "A senha deve conter pelo menos sete caracteres!";
+    else if (!validPass.test(password)) warning.innerHTML += "A senha só pode conter letras, números, \"-\", \"_\", \"!\" e \"?\".";
+
+    if (warning.innerHTML.length == 0) request(url, "post", formData, false, success, (request) => warning.innerHTML = `Ocorreu um erro (${request.status}).`);
+}
 
 document.getElementById("sign-form").addEventListener("submit", (e) =>
 {
     e.preventDefault();
 
-    const form = new FormData(e.target);
-    const username = form.get("username"), password = form.get("password");
-    warningSign.innerHTML = "";
-
-    if (username.length < 3) warningSign.innerHTML += "O nome de usuário deve conter pelo menos três caracteres!<br />";
-    else if (!validUser.test(username)) warningSign.innerHTML += "O nome de usuário só pode conter letras, números, \"-\" e \"_\".<br />";
-
-    if (password.length < 7) warningSign.innerHTML += "A senha deve conter pelo menos sete caracteres!";
-    else if (!validPass.test(password)) warningSign.innerHTML += "A senha só pode conter letras, números, \"-\", \"_\", \"!\" e \"?\".";
-
-    if (warningSign.innerHTML.length == 0)
+    formSubmit("api/create_account", e.target, warningSign, (request) =>
     {
-        request("api/create_account", "post", form, false, (request) =>
+        switch (request.responseText)
         {
-            switch (request.responseText)
-            {
-                case '0': warningSign.innerHTML = "Este nome de usuário já foi pego!"; break;
-                case '1': location.reload(); break;
-                case '2': warningSign.innerHTML = "Nome ou senha inválidos!"; break;
-            }
-        });
-    }
+            case '0': warningSign.innerHTML = "Este nome de usuário já foi pego!"; break;
+            case '1': location.reload(); break;
+            case '2': warningSign.innerHTML = "Nome ou senha inválidos!"; break;
+        }
+    });
 });
